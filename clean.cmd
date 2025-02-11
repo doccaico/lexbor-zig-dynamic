644f454f50a6fc76971580dedb7a2205f5c83d13
doccaico <hash_ext_luna@yahoo.co.jp>
2025-02-11 11:17:28 +0900

    updated clean.md

--- clean.cmd
+++ clean.cmd
@@ -2,10 +2,8 @@
 
 setlocal
 
-rmdir /S /Q c:\Users\doccaico\AppData\Local\zig
+rmdir /S /Q %USERPROFILE%\AppData\Local\zig
 rmdir /S /Q .zig-cache
 rmdir /S /Q zig-out
 
 endlocal
-
-REM vim: ft=dosbatch fenc=cp932 ff=dos
